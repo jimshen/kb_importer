d3098b5c9f5ad9faad51ad5543faf4eeabfe246b
jimshen <jimshen@gmail.com>
2016-03-28 13:49:42 +0800

    添加文件用于清理实验室名称

---
+++ cl.py
@@ -0,0 +1,24 @@
+﻿# -*- coding: utf-8 -*-
+__author__ = 'jimshen'
+import os
+import re
+import chardet
+
+file = open("d:/1.txt","r")
+fw= open("d:/res.txt","w")
+for line in file:
+    ot={}
+    arr = line.strip().split(";")
+    for a in arr:
+        if re.compile(r"(.*N6\-\d+).*").match(a)<>None:
+            ot[re.compile(r"(.*N6\-\d+).*").match(a).group(1)]=1
+        if re.compile(ur'.*(敏行楼\d+).*'.encode('utf-8')).match(a)<>None:
+            ot[re.compile(ur'.*(敏行楼\d+).*'.encode('utf-8')).match(a).group(1)]=1
+    if not ot:
+        print
+    else:
+        for key in ot:
+            print key.decode('utf-8'),
+        print
+file.close()
+fw.close()
